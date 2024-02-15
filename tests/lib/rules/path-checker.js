4558a9fee0512b64b7d149392f4694a134f205ab
Archy Fut <futsmit@yandex.ru>
2024-02-15 12:00:32 +0300

    Added support alias

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -16,16 +16,36 @@ const rule = require("../../../lib/rules/path-checker"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion:6,
+    sourceType: "module",
+  },
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename:"C:\\Programming\\learning\\prod-project-react\\src\\entities\\Article",
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
-      code: "fail",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename:"C:\\Programming\\learning\\prod-project-react\\src\\entities\\Article",
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
+      options:[
+        {
+          alias: '@',
+        }
+      ]
+    },
+    {
+      filename:"C:\\Programming\\learning\\prod-project-react\\src\\entities\\Article",
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
     },
   ],
 });
